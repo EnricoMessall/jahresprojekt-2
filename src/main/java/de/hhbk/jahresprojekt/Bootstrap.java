78daa280f42e3a52da3b8d465f2dab06c337cc10
Enrico Messall <enrico.messall@arematics.com>
2021-05-25 11:09:58 +0200

    Initial Commit

---
+++ src/main/java/de/hhbk/jahresprojekt/Bootstrap.java
@@ -0,0 +1,25 @@
+package de.hhbk.jahresprojekt;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class Bootstrap extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        primaryStage.setTitle("Jahresprojekt");
+        primaryStage.show();
+
+        Pane dashboard = FXMLLoader.load(Bootstrap.class.getResource("dashboard.fxml"));
+        primaryStage.setScene(new Scene(dashboard));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
