8725c6e0d10e214c3b6b0db5cec21e0f83496cfc
Admin <bastian.gruetz@gmail.com>
2021-06-02 13:10:37 +0200

    controller

--- src/main/java/de/hhbk/jahresprojekt/view_controller/FinanzeverwaltungController.java
+++ src/main/java/de/hhbk/jahresprojekt/view_controller/FinanzeverwaltungController.java
@@ -1,4 +1,34 @@
 package de.hhbk.jahresprojekt.view_controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
 public class FinanzeverwaltungController {
+
+    @FXML
+    private TextField receiver;
+    @FXML
+    private TextField date;
+    @FXML
+    private TextField meta;
+    @FXML
+    private ListView posten;
+    @FXML
+    private Button dashboard;
+    @FXML
+    private Button save;
+
+    @FXML
+    public void loadDashboard() {
+
+    }
+
+
+    @FXML
+    public void save() {
+
+
+    }
 }
--- src/main/java/de/hhbk/jahresprojekt/view_controller/LoginController.java
+++ src/main/java/de/hhbk/jahresprojekt/view_controller/LoginController.java
@@ -1,4 +1,28 @@
 package de.hhbk.jahresprojekt.view_controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class LoginController {
-}
+
+    @FXML
+    private TextField username;
+    @FXML
+    private PasswordField password;
+    @FXML
+    private Button dashboard;
+    @FXML
+    private Button login;
+    @FXML
+    public void login () {
+
+    }
+    @FXML
+    public void loadDashboard () {
+
+    }
+
+
+    }
