2e59cce907e2cea02dd0394233b78da36eafa97f
Admin <bastian.gruetz@gmail.com>
2021-06-02 13:57:53 +0200

    controller

---
+++ src/main/java/de/hhbk/jahresprojekt/view_controller/DashboardController.java
@@ -0,0 +1,24 @@
+package de.hhbk.jahresprojekt.view_controller;
+
+import javafx.scene.control.Button;
+
+public class DashboardController {
+
+    private Button login;
+
+    private Button usermanagement;
+
+    private Button finanzverwaltung;
+
+    public void loadLogin () {
+
+    }
+
+    public void loadUsermanagement () {
+
+    }
+
+    public void loadFinanzverwaltung () {
+
+    }
+}
