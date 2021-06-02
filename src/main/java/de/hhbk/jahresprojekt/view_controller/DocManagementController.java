78f92459ef3d3c516cb93ce0dd6987a03a466df7
Admin <bastian.gruetz@gmail.com>
2021-06-02 18:18:12 +0200

    controller

---
+++ src/main/java/de/hhbk/jahresprojekt/view_controller/DocManagementController.java
@@ -0,0 +1,58 @@
+package de.hhbk.jahresprojekt.view_controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+
+public class DocManagementController {
+    @FXML
+    private ListView lv_docs;
+    @FXML
+    private ListView lv_files;
+    @FXML
+    private ListView lv_things;
+    @FXML
+    private Button show_doc;
+    @FXML
+    private Button delete_doc;
+    @FXML
+    private Button open_file;
+    @FXML
+    private Button delete_file;
+    @FXML
+    private Button dashboard;
+    @FXML
+    private Button show_things;
+    @FXML
+    private Button delete_things;
+    @FXML
+    public void show_doc() {
+
+    }
+    @FXML
+    public void delete_doc () {
+
+    }
+    @FXML
+    public void open_file () {
+
+    }
+    @FXML
+    public void delete_file () {
+
+    }
+    @FXML
+    public void loadDashboard () {
+
+    }
+    @FXML
+    public void show_things () {
+
+    }
+    @FXML
+    public void delete_things () {
+
+    }
+
+
+}
